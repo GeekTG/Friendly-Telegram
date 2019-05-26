760e775e0fb379bbc41b780cc652ef117b9d049a
Hackintosh <hackintosh5@wildfyre.net>
2019-05-26 18:45:09 +0100

    Add purge module

---
+++ modules/purge.py
@@ -0,0 +1,24 @@
+from .. import loader, main
+import logging
+from telethon import functions
+
+def register(cb):
+    logging.debug('Registering %s', __file__)
+    cb(PurgeMod())
+
+class PurgeMod(loader.Module):
+    def __init__(self):
+        logging.debug('%s started', __file__)
+        self.commands = {'purge':self.purgecmd}
+        self.config = {}
+        self.name = "PurgeMod"
+    async def purgecmd(self, message):
+        if not message.is_reply:
+            await message.edit("From where shall I purge?")
+            return
+        msgs = [msg.id async for msg in main.client.iter_messages(
+            entity=message.to_id,
+            min_id=message.reply_to_msg_id,
+            reverse=True
+        )]
+        await main.client.delete_messages(message.to_id, msgs+[message.reply_to_msg_id])

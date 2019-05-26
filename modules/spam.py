87ca63a6e3bedc1e82a3bed408ab159c4b6c118e
Hackintosh <hackintosh5@wildfyre.net>
2019-05-26 18:44:55 +0100

    Add feature to spam via forwarding

--- modules/spam.py
+++ modules/spam.py
@@ -12,14 +12,18 @@ class SpamMod(loader.Module):
         self.config = {}
         self.name = "Spammer"
     async def spamcmd(self, message):
+        use_reply = False
         args = utils.get_args(message)
         logging.debug(args)
         if len(args) == 0:
             await message.edit("U wot? I need something to spam")
             return
         if len(args) == 1:
-            await message.edit("Go spam urself m8")
-            return
+            if message.is_reply:
+                use_reply = True
+            else:
+                await message.edit("Go spam urself m8")
+                return
         count = args[0]
         spam = ' '.join(args[1:])
         try:
@@ -32,6 +36,13 @@ class SpamMod(loader.Module):
             return
         await message.delete()
         i = 0
-        while i < count:
-            await asyncio.gather(*[main.client.send_message(message.to_id, str(spam)) for x in range(min(count, 100))])
-            i += 100
+        if use_reply:
+            reply = await message.get_reply_message()
+            logging.debug(reply)
+            while i < count:
+                await asyncio.gather(*[reply.forward_to(message.to_id) for x in range(min(count, 100))])
+                i += 100
+        else:
+            while i < count:
+                await asyncio.gather(*[main.client.send_message(message.to_id, str(spam)) for x in range(min(count, 100))])
+                i += 100

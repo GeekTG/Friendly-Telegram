1c5fdc0ad6b08cf224c46fa5c4f00a6690637069
penn5 <penn5@users.noreply.github.com>
2019-05-26 21:53:48 +0100

    Better insults

--- modules/insult.py
+++ modules/insult.py
@@ -15,8 +15,16 @@ class InsultMod(loader.Module):
         self.name = "InsultMod"
 
     async def insultcmd(self, message):
-        adjectives = ["salty ", "fat ", "fucking ", "shitty ", "stupid ", "retarded"]
-        verbs = ["cunt ", "pig ", "pedophile ", "alpha male ", "retard", "ass licker "]
-        insult = "You're a " + random.choice(adjectives) + random.choice(verbs)
+        adjectives_start = ["salty", "fat", "fucking", "shitty", "stupid", "retarded"]
+        adjectives_mid = ["little", None]
+        nouns = ["cunt", "pig", "pedophile", "alpha male", "retard", "ass licker"]
+        starts = ["You're a", "You"]
+        ends = ["!!!!", "!", ""]
+        start = random.choice(starts)
+        adjective_start = random.choice(adjectives_start)
+        adjective_mid = random.choice(adjectives_mid)
+        noun = random.choice(noun)
+        end = random.choice(ends)
+        insult = start + " " + adjective_start + " " + adjective_mid + (" " if adjective_mid else "") + noun + end
         await message.edit(insult)
 

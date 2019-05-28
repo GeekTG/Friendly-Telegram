37d2cc9a1c0b7d61cd827d818c2a0d8446e19c7a
Hackintosh <hackintosh5@wildfyre.net>
2019-05-28 14:59:18 +0100

    Remove test code

--- config.py
+++ config.py
@@ -2,4 +2,4 @@ MODULES_NAME = 'modules'
 TYPE_CHAR = "▒"
 GENUIS_API_TOKEN = "API_TOKEN"
 
-GIT_PULL_COMMAND=["git", "pull", "--rebase=error for testing!"]
+GIT_PULL_COMMAND=["git", "pull", "--ff-only"]

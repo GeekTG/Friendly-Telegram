04654bb5bc78f9506345c07fd7c998548836a3d1
Hackintosh <hackintosh5@wildfyre.net>
2019-05-28 19:12:09 +0100

    Move typewriter to new config system

--- modules/typer.py
+++ modules/typer.py
@@ -1,4 +1,4 @@
-from .. import loader, utils, main, config
+from .. import loader, utils
 from telethon.errors.rpcerrorlist import *
 import logging, time, asyncio
 
@@ -10,7 +10,7 @@ class TyperMod(loader.Module):
     def __init__(self):
         logging.debug('%s started', __file__)
         self.commands = {'type':self.typecmd}
-        self.config = {}
+        self.config = {"TYPE_CHAR":"▒"}
         self.name = "Typewriter"
         self.help = "Makes your messages type slower"
 
@@ -18,7 +18,7 @@ class TyperMod(loader.Module):
         a = utils.get_args_raw(message)
         m = ""
         for c in a:
-            m += config.TYPE_CHAR
+            m += self.config["TYPE_CHAR"]
             message = await update_message(message, m)
             await asyncio.sleep(0.04)
             m = m[:-1]+c
@@ -36,6 +36,6 @@ async def update_message(message, m):
             message.delete()
         except:
             logging.warning("message gone!") # WTF? It's really not here...
-        message = await main.client.send_message(message.to_id, m) # Make a new one.
+        message = await message.client.send_message(message.to_id, m) # Make a new one.
 
     return message

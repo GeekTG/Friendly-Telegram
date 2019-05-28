c625723aed1a299163ba98c54a9a330829ff9f6c
Hackintosh <hackintosh5@wildfyre.net>
2019-05-28 12:30:18 +0100

    Add .del command to purge module

--- modules/purge.py
+++ modules/purge.py
@@ -9,7 +9,7 @@ def register(cb):
 class PurgeMod(loader.Module):
     def __init__(self):
         logging.debug('%s started', __file__)
-        self.commands = {'purge':self.purgecmd}
+        self.commands = {'purge':self.purgecmd, "del":self.delcmd}
         self.config = {}
         self.name = "Purge"
         self.help = "Deletes things quickly"
@@ -24,3 +24,13 @@ class PurgeMod(loader.Module):
             reverse=True
         )]
         await message.client.delete_messages(message.to_id, msgs+[message.reply_to_msg_id])
+
+    async def delcmd(self, message):
+        await message.delete()
+        if not message.is_reply:
+            iter = message.client.iter_messages(
+                entity=message.to_id
+            )
+            await (await iter.__anext__()).delete()
+        else:
+            await (await message.get_reply_message()).delete()

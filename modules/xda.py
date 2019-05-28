3e43415824ed5d5344a26dfa02113e07a8c8103a
Ben Grisdale <bengrisdale123@gmail.com>
2019-05-28 19:50:49 +0000

    Add more strings for XDA module

--- modules/xda.py
+++ modules/xda.py
@@ -8,6 +8,12 @@ def register(cb):
 RANDOM_WORDS = ["sur", "Sir", "bro", "yes", "no", "bolte", "bolit", "bholit", "volit", "mustah", "fap", "lit", "lmao", "iz", "jiosim", "ijo", "nut", "workz", "workang"]
 WEIGHT_WORDS = [6    , 6,     6    , 5    , 5   , 2      , 2      , 3       , 3      , 4       , 5    , 3    , 6     , 7   , 8       , 4    , 7    , 4      , 4        ]
 
+RANDOM_WORDS += ["flashabl zip", "bateri", "bacup", "bad englis", "sar", "treble wen", "bro send gsi", "fox bug", "fine and u", "best room", "fix", "bug fox sir"]
+WEIGHT_WORDS += [6             , 6       , 6      , 5           , 5   ,  2           , 2             , 3        , 3           , 4          , 5    , 3            ]
+
+RANDOM_WORDS += ["plz make room", "andreid pai", "when", "port", "mtk", "send moni", "bad rom"]
+WEIGHT_WORDS += [3              , 2            , 4     , 5     , 3    , 3          , 2        ] 
+
 class TestMod(loader.Module):
     def __init__(self):
         logging.debug('%s started', __file__)

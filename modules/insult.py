e73071d38abb497fadb07777f2b1b45b5d80a3ba
clecho52 <nikolayordanov2667@gmail.com>
2019-06-05 21:27:55 +0300

    Update insult.py

--- modules/insult.py
+++ modules/insult.py
@@ -16,9 +16,9 @@ class InsultMod(loader.Module):
 
     async def insultcmd(self, message):
         adjectives_start = ["salty", "fat", "fucking", "shitty", "stupid", "retarded", "gay","self-concious","tiny"]
-        adjectives_mid = ["little", "vitamin D deficient", "idiotic", "incredibly stupid",]
-        nouns = ["cunt", "pig", "pedophile", "beta male","bottom" "retard", "ass licker", "cunt nugget", "PENIS", "dickhead", "flute","idiot","motherfucker
-,"loner"]
+        adjectives_mid = ["little", "vitamin D deficient", "idiotic", "incredibly stupid"]
+        nouns = ["cunt", "pig", "pedophile", "beta male","bottom" "retard", "ass licker", "cunt nugget", "PENIS", "dickhead", "flute","idiot","motherfucker",
+"loner"]
         starts = ["You're a", "You", "Fuck off you","Actually die you", "Listen up you", "What the fuck is wrong with you, you"]
         ends = ["!!!!", "!", ""]
         start = random.choice(starts)

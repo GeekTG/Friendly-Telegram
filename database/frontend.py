1e0b4b7b29d15de42f42260bcb7a1faaf3613783
Hackintosh <hackintosh5@wildfyre.net>
2019-06-07 08:24:47 +0100

    Fix DB bugs and refactor stuff

--- database/frontend.py
+++ database/frontend.py
@@ -7,11 +7,23 @@ class Database():
         await self._backend.init()
         db = await self._backend.do_download()
         if db != None:
-            self._db = json.loads(db)
+            try:
+                self._db = json.loads(db)
+            except:
+                # Don't worry if its corrupted. Just set it to {} and let it be fixed on next upload
+                self._db = {}
         else:
             self._db = {}
     def get(self, owner, key):
-        return self._db[owner+"/"+key]
+        try:
+            return self._db[owner][key]
+        except KeyError:
+            return None
+
     def set(self, owner, key, value):
-        self._db[owner+"/"+key] = value
+        self._ensure_owned(owner)
+        self._db[owner][key] = value
         asyncio.create_task(self._backend.do_upload(json.dumps(self._db)))
+    def _ensure_owned(self, owner):
+        if not owner in self._db.keys():
+            self._db[owner] = {}

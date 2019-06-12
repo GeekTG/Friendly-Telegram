6df6fa3531d13bfccb8d1c1b22b7686f478cd72c
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-06-12 19:21:17 +0100

    Add dump code

--- modules/test.py
+++ modules/test.py
@@ -1,4 +1,4 @@
-from .. import loader
+from .. import loader, utils
 
 def register(cb):
     cb(TestMod())
@@ -6,7 +6,7 @@ def register(cb):
 class TestMod(loader.Module):
     """Self-tests"""
     def __init__(self):
-        self.commands = {'ping':self.pingcmd}
+        self.commands = {'ping':self.pingcmd, 'dump':self.dumpcmd}
         self.config = {}
         self.name = "Tester"
 
@@ -14,3 +14,8 @@ class TestMod(loader.Module):
         """Does nothing"""
         await message.edit('Pong')
 
+    async def dumpcmd(self, message):
+        """Use in reply to get a dump of a message"""
+        if not message.is_reply:
+            return
+        await message.edit(utils.escape_html((await message.get_reply_message()).stringify()), parse_mode="HTML")

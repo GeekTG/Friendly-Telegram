2f47582fa527f55bea06d2b6fa6b377482f1fa8a
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-06-28 17:44:27 +0100

    Rename update commands

--- friendly-telegram/modules/updater.py
+++ friendly-telegram/modules/updater.py
@@ -11,18 +11,18 @@ def register(cb):
 class UpdaterMod(loader.Module):
     """Updates itself"""
     def __init__(self):
-        self.commands = {'selfupdate': self.updatecmd, "pull": self.pullcmd}
+        self.commands = {'restart': self.restartcmd, "download": self.downloadcmd}
         self.config = {"selfupdatechat": -1, "selfupdatemsg": -1, "GIT_PULL_COMMAND": ["git", "pull", "--ff-only"]}
         self.name = "Updater"
 
-    async def updatecmd(self, message):
+    async def restartcmd(self, message):
         """Restarts the userbot"""
-        await message.edit('Updating...')
+        await message.edit('Restarting...')
         logger.debug("Self-update. " + sys.executable + " -m " + utils.get_base_dir())
         atexit.register(functools.partial(restart, "--config", "selfupdatechat", "--value", str(utils.get_chat_id(message)), "--config", "selfupdatemsg", "--value", str(message.id)))
         await message.client.disconnect()
 
-    async def pullcmd(self, message):
+    async def downloadcmd(self, message):
         """Downloads userbot updates"""
         await message.edit("Downloading...")
         gitproc = await asyncio.create_subprocess_exec(*self.config["GIT_PULL_COMMAND"], stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE, cwd=utils.get_base_dir())
@@ -30,11 +30,11 @@ class UpdaterMod(loader.Module):
         if gitproc.returncode != 0:
             await message.edit("Error!\nStdout:\n<code>"+utils.escape_html(out.decode("utf-8"))+"</code>\nStderr:\n<code>"+utils.escape_html(err.decode("utf-8"))+"</code>")
         else:
-            await message.edit("Downloaded! Use <code>.selfupdate</code> to restart.")
+            await message.edit("Downloaded! Use <code>.restart</code> to restart.")
     async def client_ready(self, client, db):
         if self.config["selfupdatemsg"] >= 0:
             logger.debug("Self update successful! Edit message: "+str(self.config))
-            await client.edit_message(self.config["selfupdatechat"], self.config["selfupdatemsg"], "Update successful!")
+            await client.edit_message(self.config["selfupdatechat"], self.config["selfupdatemsg"], "Restart successful!")
 
 def restart(*args):
     os.execl(sys.executable, sys.executable, "-m", utils.get_base_dir(), *args)

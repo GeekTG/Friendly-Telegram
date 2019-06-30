07e4755020d4c56cd6366a576cdd60289c186576
loque036 <loque036@gmail.com>
2019-06-30 12:58:17 +0200

    added new words

--- friendly-telegram/modules/xda.py
+++ friendly-telegram/modules/xda.py
@@ -7,11 +7,11 @@ def register(cb):
 RANDOM_WORDS = ["sur", "Sir", "bro", "yes", "no", "bolte", "bolit", "bholit", "volit", "mustah", "fap", "lit", "lmao", "iz", "jiosim", "ijo", "nut", "workz", "workang"]
 WEIGHT_WORDS = [6    , 6,     6    , 5    , 5   , 2      , 2      , 3       , 3      , 4       , 5    , 3    , 6     , 7   , 8       , 4    , 7    , 4      , 4        ]
 
-RANDOM_WORDS += ["flashabl zip", "bateri", "bacup", "bad englis", "sar", "treble wen", "gsi", "fox bag", "bag fox", "fine", "bast room", "fax"]
-WEIGHT_WORDS += [6             , 6       , 6      , 5           , 5    ,  2          , 6    , 3        , 3        , 4     , 5          , 3    ]
+RANDOM_WORDS += ["flashabl zip", "bateri", "bacup", "bad englis", "sar", "treble wen", "gsi", "fox bag", "bag fox", "fine", "bast room", "fax", "trable"]
+WEIGHT_WORDS += [6             , 6       , 6      , 5           , 5    ,  2          , 6    , 3        , 3        , 4     , 5          , 3    , 3       ]
 
-RANDOM_WORDS += ["plz make room", "andreid pai", "when", "port", "mtk", "send moni", "bad rom", "dot", "kenzo", "rr", "linage"]
-WEIGHT_WORDS += [3              , 2            , 4     , 5     , 3    , 3          , 2        , 4    , 4      , 4   , 4       ]
+RANDOM_WORDS += ["plz make room", "andreid pai", "when", "port", "mtk", "send moni", "bad rom", "dot", "kenzo", "rr", "linage", "arrows", "kernal"]
+WEIGHT_WORDS += [3              , 2            , 4     , 5     , 3    , 3          , 2        , 4    , 4      , 4   , 4       , 4       , 4       ]
 
 # Workaround for 3.5
 WORDS_WEIGHTED = [RANDOM_WORDS[i] for i in range(len(RANDOM_WORDS)) for dummy in range(WEIGHT_WORDS[i])]

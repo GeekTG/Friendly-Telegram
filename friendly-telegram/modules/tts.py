28789b917668f50d48601b93b78d8b57cb887440
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-08-13 19:36:07 +0100

    Add tts module (thanks @bengris32)

---
+++ friendly-telegram/modules/tts.py
@@ -0,0 +1,29 @@
+import logging
+
+from gtts import gTTS
+
+from io import BytesIO
+
+from .. import loader, utils
+
+def register(cb):
+    cb(TTSMod())
+
+class TTSMod(loader.Module):
+    def __init__(self):
+        self.commands = {"tts":self.ttscmd}
+        self.config = {}
+        self.name = "Text to speech"
+
+    async def ttscmd(self, message):
+        args = utils.get_args_raw(message)
+        if not args:
+            args = (await message.get_reply_message()).message
+
+        tts = await utils.run_sync(gTTS, args)
+        voice = BytesIO()
+        tts.write_to_fp(voice)
+        voice.seek(0)
+        voice.name = "voice.mp3"
+
+        await utils.answer(message, voice, voice_note=True)

d0d1a833e396ca87e65551ef27bd3f49985b7ff5
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-08-15 18:21:24 +0100

    Better restoration stuff

--- friendly-telegram/modules/recentactions.py
+++ friendly-telegram/modules/recentactions.py
@@ -15,9 +15,8 @@ class RecentActionsMod(loader.Module):
         self.name = "Recent Actions"
 
     async def yourcmd(self, message):
-        """Restores deleted messages sent after replied message"""
+        """Restores deleted messages sent after replied message (optionally specify how many to recover)"""
         msgs = message.client.iter_admin_log(message.to_id, delete=True)
-        print(msgs)
         if not message.is_reply:
             await utils.answer(message, "<code>Reply to a message to specify where to start</code>")
         target = (await message.get_reply_message()).date
@@ -31,9 +30,16 @@ class RecentActionsMod(loader.Module):
                 ret += [msg]
         except telethon.errors.rpcerrorlist.ChatAdminRequiredError:
             await utils.answer(message, "<code>Admin is required to read deleted messages</code>")
+        args = utils.get_args(message)
+        if len(args) > 0:
+            try:
+                count = int(args[0])
+                ret = ret[0-count:]
+            except ValueError:
+                pass
         for msg in reversed(ret):
             orig = msg.original.action.message
             deldate = msg.original.date.isoformat()
             origdate = orig.date.isoformat()
-            await message.respond(f"=== Deleted message recovered. Originally sent at {origdate}, deleted at {deldate}. ===")
+            await message.respond(f"=== Deleted message {orig.id} recovered. Originally sent at {origdate}, deleted at {deldate}. ===")
             await message.respond(orig)

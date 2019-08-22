d57349174d5c7c67cbb78b82b120027f4bd5acc6
penn5 <penn5@users.noreply.github.com>
2019-08-22 12:51:17 +0100

    Fix .eval with new formatting

--- friendly-telegram/modules/python.py
+++ friendly-telegram/modules/python.py
@@ -83,7 +83,7 @@ class PythonMod(loader.Module):
         """.eval <expression>
            Evaluates python code"""
         ret = _("Evaluated expression <code>{}</code> and it returned <code>{}</code>")
-        ret.format(utils.escape_html(utils.get_args_raw(message)), utils.escape_html(await meval(utils.get_args_raw(message), **self.getattrs(message))))
+        ret = ret.format(utils.escape_html(utils.get_args_raw(message)), utils.escape_html(await meval(utils.get_args_raw(message), **self.getattrs(message))))
         await message.edit(ret)
 
     async def execcmd(self, message):

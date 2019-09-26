17643b4161a49047032c115fc72e08f7fcf32ea1
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-09-26 14:32:07 +0100

    Add warning about using 3.8

--- friendly-telegram/__main__.py
+++ friendly-telegram/__main__.py
@@ -18,6 +18,8 @@ import sys
 if sys.version_info < (3, 5, 0):  # Minimum version - asyncio got major revamp and so did importlib
     print("Error: you must use at least Python version 3.5.0")
 else:
+    if sys.version_info >= (3, 8, 0):
+        print("Warning: you are using an untested Python version")
     if __package__ != "friendly-telegram":  # In case they did python __main__.py
         print("Error: you cannot run this as a script; you must execute as a package")
     else:

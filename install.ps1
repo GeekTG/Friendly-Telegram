0153fac3dd692fb1b2b02af5ac18e1a3b3929501
penn5 <penn5@users.noreply.github.com>
2019-10-21 12:27:11 +0100

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -27,8 +27,17 @@ Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe
 Write-Output("Installing Python...")
 Start-Process (Join-Path $env:TEMP "python-installer.exe") @("/quiet"; "InstallAllUsers=0"; "PrependPath=1"; "Include_test=0"; "InstallLauncherAllUsers=0") -Wait
 Write-Output("Locating Git...")
-$ret = Invoke-RestMethod -Uri "https://api.github.com/repos/git-for-windows/git/releases/latest" -Headers @{'User-Agent'='friendly-telegram installer'}
-$asset_id = $ret.assets | Where {$_.name -Match ("^Git-[0-9]+\.[0-9]+\.[0-9]+-" +  (Get-WmiObject -Class Win32_OperatingSystem -ComputerName $env:computername -ea 0).OSArchitecture + ".exe$")} | % {$_.id}
+$ret = Invoke-RestMethod -Uri "https://api.github.com/repos/git-for-windows/git/releases" -Headers @{'User-Agent'='friendly-telegram installer'}
+foreach ($release in $ret) {
+    $asset_id = $release.assets | Where {$_.name -Match ("^Git-[0-9]+\.[0-9]+\.[0-9]+-" +  (Get-WmiObject -Class Win32_OperatingSystem -ComputerName $env:computername -ea 0).OSArchitecture + ".exe$")} | % {$_.id}
+    if (-not [string]::IsNullOrEmpty($asset_id)) {
+        break
+    }
+}
+if ([string]::IsNullOrEmpty($asset_id)) {
+    Write-Error "Unable to locate Git"
+    exit
+}
 $download_url = "https://api.github.com/repos/git-for-windows/git/releases/assets/" + $asset_id
 Write-Output("Downloading Git...")
 Invoke-WebRequest -Uri $download_url -OutFile (Join-Path $env:TEMP "git-scm-installer.exe") -Headers @{'User-Agent'='friendly-telegram installer'; 'Accept'='application/octet-stream'}

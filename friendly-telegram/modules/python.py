a99c632f76243fa446dc98c9dd4dbb0f45525944
Hackintosh 5 <hackintosh5@wildfyre.net>
2019-10-27 20:20:20 +0000

    eval: modularise core code

--- friendly-telegram/modules/python.py
+++ friendly-telegram/modules/python.py
@@ -17,12 +17,11 @@
 #    along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 import logging
-import ast
 import traceback
 import sys
 import itertools
 import types
-import importlib
+from meval import meval
 
 import telethon
 
@@ -33,64 +32,6 @@ logger = logging.getLogger(__name__)
 
 def register(cb):
     cb(PythonMod())
-
-
-# We dont modify locals VVVV ; this lets us keep the message available to the user-provided function
-async def meval(code, **kwargs):
-    # This function is released in the public domain. Feel free to kang it (although I like credit)
-    # Note to self: please don't set globals here as they will be lost.
-    # Don't clutter locals
-    locs = {}
-    # Restore globals later
-    globs = globals().copy()
-    # This code saves __name__ and __package into a kwarg passed to the function.
-    # It is set before the users code runs to make sure relative imports work
-    global_args = "_globs"
-    while global_args in globs.keys():
-        # Make sure there's no name collision, just keep prepending _s
-        global_args = "_" + global_args
-    kwargs[global_args] = {}
-    for glob in ["__name__", "__package__"]:
-        # Copy data to args we are sending
-        kwargs[global_args][glob] = globs[glob]
-
-    root = ast.parse(code, "exec")
-    code = root.body
-    if isinstance(code[-1], ast.Expr):  # If we can use it as a lambda return (but multiline)
-        code[-1] = ast.copy_location(ast.Return(code[-1].value), code[-1])  # Change it to a return statement
-    # globals().update(**<global_args>)
-    glob_copy = ast.Expr(ast.Call(func=ast.Attribute(value=ast.Call(func=ast.Name(id="globals", ctx=ast.Load()),
-                                                                    args=[], keywords=[]),
-                                                     attr="update", ctx=ast.Load()),
-                                  args=[], keywords=[ast.keyword(arg=None,
-                                                                 value=ast.Name(id=global_args, ctx=ast.Load()))]))
-    ast.fix_missing_locations(glob_copy)
-    code.insert(0, glob_copy)
-    args = []
-    for a in list(map(lambda x: ast.arg(x, None), kwargs.keys())):
-        ast.fix_missing_locations(a)
-        args += [a]
-    args = ast.arguments(args=[], vararg=None, kwonlyargs=args, kwarg=None, defaults=[],
-                         kw_defaults=[None for i in range(len(args))])
-    if int.from_bytes(importlib.util.MAGIC_NUMBER[:-2], 'little') >= 3410:
-        args.posonlyargs = []
-    fun = ast.AsyncFunctionDef(name="tmp", args=args, body=code, decorator_list=[], returns=None)
-    ast.fix_missing_locations(fun)
-    mod = ast.parse("")
-    mod.body = [fun]
-    comp = compile(mod, "<string>", "exec")
-
-    exec(comp, {}, locs)
-
-    r = await locs["tmp"](**kwargs)
-    if isinstance(r, types.CoroutineType):
-        r = await r  # workaround for 3.5
-    try:
-        globals().clear()
-        # Inconsistent state
-    finally:
-        globals().update(**globs)
-    return r
 
 
 class PythonMod(loader.Module):

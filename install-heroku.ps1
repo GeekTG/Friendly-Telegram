3b364ca426b6075446fdea2d3b08029e0104aa22
David Hu <d4v1d_4n0@protonmail.ch>
2021-01-18 07:12:35 +0000

    Update install-heroku.ps1 Update Python version

--- install-heroku.ps1
+++ install-heroku.ps1
@@ -23,7 +23,7 @@ if (Test-Path "friendly-telegram" -PathType Container) {
 }
 
 Write-Output("Downloading Python...")
-Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.7.4/python-3.7.4.exe" -OutFile (Join-Path $env:TEMP "python-installer.exe")
+Invoke-WebRequest -Uri "https://www.python.org/ftp/python/3.9.1/python-3.9.1.exe" -OutFile (Join-Path $env:TEMP "python-installer.exe")
 Write-Output("Installing Python...")
 Start-Process (Join-Path $env:TEMP "python-installer.exe") @("/quiet"; "InstallAllUsers=0"; "PrependPath=1"; "Include_test=0"; "InstallLauncherAllUsers=0") -Wait
 Write-Output("Locating Git...")

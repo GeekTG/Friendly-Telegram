f9e21db083904bddc0a4402ad0e65c7f0dde0dc1
fast-geek <rodiongudz.services@gmail.com>
2021-03-28 00:30:36 +0300

    Code update

--- friendly-telegram/modules/test.py
+++ friendly-telegram/modules/test.py
@@ -16,6 +16,7 @@
 
 import logging
 import time
+import speedtest
 
 from io import BytesIO
 
@@ -56,7 +57,13 @@ class TestMod(loader.Module):
                "logs_unsafe": ("<b>Warning: running this command may reveal personal or dangerous information. "
                                "You can write</b> <code>{}</code> <b>at the end to accept the risks</b>"),
                "logs_force": "FORCE_INSECURE",
-               "suspend_invalid_time": "<b>Invalid time to suspend</b>"}
+               "suspend_invalid_time": "<b>Invalid time to suspend</b>",
+               "running": "<b>Running speedtest...</b>",
+               "results": "<b>Speedtest Results:</b>",
+               "results_download": "<b>Download:</b> <code>{}</code> <b>MiB/s</b>",
+               "results_upload": "<b>Upload:</b> <code>{}</code> <b>MiB/s</b>",
+               "results_ping": "<b>Ping:</b> <code>{}</code> <b>ms</b>"
+               }
 
     @loader.test(func=dumptest)
     async def dumpcmd(self, message):
@@ -114,6 +121,31 @@ class TestMod(loader.Module):
         end = datetime.now()
         ms = (end - start).microseconds / 1000
         await utils.answer(message, "<b>Ping:</b> <code>{}ms</code>".format(ms))
+
+    async def speedtestcmd(self, message):
+        """Tests your internet speed"""
+        await utils.answer(message, self.strings("running", message))
+        args = utils.get_args(message)
+        servers = []
+        for server in args:
+            try:
+                servers += [int(server)]
+            except ValueError:
+                logger.warning("server failed")
+        results = await utils.run_sync(self.speedtest, servers)
+        ret = self.strings("results", message) + "\n\n"
+        ret += self.strings("results_download", message).format(round(results["download"] / 2**20, 2)) + "\n"
+        ret += self.strings("results_upload", message).format(round(results["upload"] / 2**20, 2)) + "\n"
+        ret += self.strings("results_ping", message).format(round(results["ping"], 2)) + "\n"
+        await utils.answer(message, ret)
+
+    def speedtest(self, servers):
+        speedtester = speedtest.Speedtest()
+        speedtester.get_servers(servers)
+        speedtester.get_best_server()
+        speedtester.download(threads=None)
+        speedtester.upload(threads=None)
+        return speedtester.results.dict()
 
     async def client_ready(self, client, db):
         self.client = client

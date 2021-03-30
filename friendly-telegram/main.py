ef97e309b0b8c3960c7c55f69c8b0560a681c435
Stas Tirtichniy (Стас Тыртычный) <67735535+Fl1yd@users.noreply.github.com>
2021-03-30 09:17:38 +0600

    fixed ValueError; я не знаю кто это сделал, но если это нужно вернуть, я верну
    кто увидил тово люблю

--- friendly-telegram/main.py
+++ friendly-telegram/main.py
@@ -219,7 +219,7 @@ def main():  # noqa: C901
                 if platform.system() == "Linux" and not os.path.exists("/etc/os-release"):
                     print(f"Please visit http://localhost:{port}")
                 else:
-                    ipaddress, port = pymyip.get_ip()
+                    ipaddress = pymyip.get_ip()
                     print(f"Please visit http://{ipaddress}:{port} or http://localhost:{port}")
             loop.run_until_complete(web.wait_for_api_token_setup())
             api_token = web.api_token

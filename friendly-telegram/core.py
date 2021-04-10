d495bcae79cb0faac01501e6b5234ee9e7f93d87
fast-geek <rodiongudz.services@gmail.com>
2021-04-10 22:43:24 +0300

    Code update, bug fixed

--- friendly-telegram/core.py
+++ friendly-telegram/core.py
@@ -23,36 +23,30 @@ class TestManager:
 		if stage > 4:
 			stage = 0
 			await self._db.set(__name__, "stage", stage)
-		if await self._client.is_bot():
-			user_id = [(await c.get_me(True)).user_id for c in self._clients if c is not self._client][0]
-			self._db.set(security.__name__, "bounding_mask", security.SUPPORT)
-			self._db.set(security.__name__, "support", [user_id])
-			await self._db.set(main.__name__, "command_prefix", ["/"])
-			return ["../botmodule.py"]
-		else:
+		if not await self._client.is_bot():
 			bot_id = [(await c.get_me(True)).user_id for c in self._clients if c is not self._client][0]
 			self._db.set(security.__name__, "bounding_mask", -1)
 			if stage == 0:
 				self._db.set(security.__name__, "bounding_mask", -1)
 				self._db.set(security.__name__, "owner", [bot_id])
-				return ["../usermodule.py"]
+				return ["usermodule.py"]
 			self._db.set(security.__name__, "owner", [-1])
 			if stage == 1:
 				self._db.set(security.__name__, "sudo", [bot_id])
-				return ["../usermodule.py"]
+				return ["usermodule.py"]
 			if stage == 2:
 				self._db.set(security.__name__, "sudo", [])
 				self._db.set(security.__name__, "support", [bot_id])
-				return ["../usermodule.py"]
+				return ["usermodule.py"]
 			if stage == 3:
 				self._db.set(security.__name__, "support", [])
-				return ["../usermodule.py"]
+				return ["usermodule.py"]
 			if stage == 4:
 				self._db.set(security.__name__, "owner", [bot_id])
 				await self._db.set(security.__name__, "bounding_mask", 1)
 				defmods = filter(lambda x: (len(x) > 3 and x[-3:] == ".py" and x[0] != "_"),
 				                 os.listdir(os.path.join(utils.get_base_dir(), loader.MODULES_NAME)))
-				return ["../test/usermodule.py"] + list(defmods)
+				return ["usermodule.py"] + list(defmods)
 
 	def should_restart(self):
 		TestManager.restart = asyncio.Future()  # reset

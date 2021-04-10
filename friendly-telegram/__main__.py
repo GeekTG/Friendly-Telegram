d1d6f2fd034d0a782630fcabd7296e6d7d7274e0
fast-geek <rodiongudz.services@gmail.com>
2021-04-10 17:36:26 +0300

    Code update

--- friendly-telegram/__main__.py
+++ friendly-telegram/__main__.py
@@ -9,8 +9,6 @@ import sys
 if sys.version_info < (3, 6, 0):
     print("Error: you must use at least Python version 3.6.0")  # pragma: no cover
 else:
-    if sys.version_info >= (3, 9, 0):
-        print("Warning: you are using an untested Python version")  # pragma: no cover
     if __package__ != "friendly-telegram":  # In case they did python __main__.py
         print("Error: you cannot run this as a script; you must execute as a package")  # pragma: no cover
     else:

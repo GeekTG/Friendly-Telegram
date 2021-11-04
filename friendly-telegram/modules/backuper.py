a2320d9d4c80dd5546ed804a2d7bedb71d2d70f3
lavender <rodiongudz.services@gmail.com>
2021-11-04 13:52:31 +0300

    Update backuper.py

--- friendly-telegram/modules/backuper.py
+++ friendly-telegram/modules/backuper.py
@@ -25,9 +25,7 @@ class BackuperMod(loader.Module):
         'backup_caption': '☝️ <b>This is your database backup. Don\'t give it to anyone</b>',
         'reply_to_file': '🚫 <b>Reply to .{} file</b>',
         'db_restored': '✅ <b>Database restored. Restarting userbot...</b>',
-        'modules_backup': '☝️ <b>Modules backup ({})</b>',
         'notes_backup': '☝️ <b>Notes backup ({})</b>',
-        'mods_restored': '📤 <b>Modules restored. Restarting userbot...</b>',
         'notes_restored': '📤 <b>Notes restored.</b>'
     }
 
@@ -58,34 +56,6 @@ class BackuperMod(loader.Module):
         self.db.save()
         # print(decoded_text)
         await utils.answer(message, self.strings('db_restored', message))
-        await self.allmodules.commands['restart'](await message.respond('_'))
-
-    async def backupmodscmd(self, message):
-        """.backupmods - Create modules backup"""
-        data = json.dumps({'loaded': self.db.get("friendly-telegram.modules.loader", "loaded_modules", []),
-                           'unloaded': self.db.get("friendly-telegram.modules.loader", "unloaded_modules", [])})
-        txt = io.BytesIO(data.encode('utf-8'))
-        txt.name = f"ftg-mods-{datetime.datetime.now().strftime('%d-%m-%Y-%H-%M')}.mods"
-        await self.client.send_file(utils.get_chat_id(message), txt, caption=self.strings('modules_backup', message).format(len(self.db.get("friendly-telegram.modules.loader", "loaded_modules", []))))
-        await message.delete()
-
-    async def restoremodscmd(self, message):
-        """.restoremods <reply to file> - Restore modules from backup"""
-        reply = await message.get_reply_message()
-        if not reply or not reply.media:
-            await utils.answer(message, self.strings('reply_to_file', message).format('mods'))
-            await asyncio.sleep(3)
-            await message.delete()
-            return
-
-        file = await message.client.download_file(reply.media)
-        decoded_text = json.loads(file.decode('utf-8'))
-        self.db.set("friendly-telegram.modules.loader",
-                    "loaded_modules", decoded_text['loaded'])
-        self.db.set("friendly-telegram.modules.loader",
-                    "unloaded_modules", decoded_text['unloaded'])
-        self.db.save()
-        await utils.answer(message, self.strings('mods_restored', message))
         await self.allmodules.commands['restart'](await message.respond('_'))
 
     async def backupnotescmd(self, message):
